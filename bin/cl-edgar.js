1a7c0e0d1c7b54f9332a06fe008a160cad0fad67
Brent Hamilton <brent@arthurkn.net>
2016-08-16 09:38:05 -0700

    more better work bin

--- bin/cl-edgar.js
+++ bin/cl-edgar.js
@@ -1,31 +1,3 @@
-const program = require('commander');
-const moment = require('moment');
+const cli = require('./../cl-edgar');
 
-const init = require('./../commands/init');
-const hasoffers = require('./../commands/hasoffers');
-const facebook = require('./../commands/facebook');
-
-program
-  .version('0.0.1');
-
-program
-  .option('-d --date <date>', 'Specify a date for the script, format YYYY-MM-DD');
-
-// init
-program
-  .command('init')
-  .description('initialize cl-edgar: install edgar, set up cron jobs, configure $PATH')
-  .action(() => {
-    init();
-  });
-
-// hasoffers
-program
-  .command('hasoffers [script]')
-  .description('run hasoffers script')
-  .action((script) => {
-    hasoffers(Object.assign(program, { script: script }));
-  });
-
-program
-  .parse(process.argv);
+cli;
